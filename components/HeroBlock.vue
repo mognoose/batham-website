4ad2e0d91ff6e57bf49feccbd854870db4435bfe
Csaba Szilagyi <csaba@lamia.fi>
2025-05-23 13:16:53 +0300

    Implement Hero that uses contentful data

--- components/HeroBlock.vue
+++ components/HeroBlock.vue
@@ -1,12 +1,10 @@
 <template>
-    <div class="hero">
-        <img ref="logo" class="logo" alt="Hero" src="../assets/batham-logo.png">
-        <h1>Batham</h1>
-    </div>
+    <div v-for="(node, index) in content" :key="index" class="hero" v-html="richTextFormat(node)" />
 </template>
 
 <script setup>
-console.log('hero');
+import { BLOCKS } from '@contentful/rich-text-types';
+import { documentToHtmlString } from '@contentful/rich-text-html-renderer';
 
 defineProps({
     content: {
@@ -14,25 +12,35 @@ defineProps({
         required: true,
     }
 })
+
+function richTextFormat(text) {
+    const options = {
+        renderNode: {
+            [BLOCKS.EMBEDDED_ASSET]: node => `<img class="logo" src="https://${node.data.target.fields.file.url}" />`,
+            [BLOCKS.HEADING_1]: node => `<h1>${node.content[0].value}</h1>`,
+        }
+    }
+    return documentToHtmlString(text, options);
+}
+
 </script>
 
 <style lang="scss" scoped>
 .hero {
     text-align: center;
 
-    .logo {
-        opacity: 0.5;
+    :deep(.logo) {
         transition-duration: 500ms;
         width: 20rem;
         margin-bottom: -4rem;
     }
 
-    h1 {
+    :deep(h1) {
         font-weight: 100;
         font-family: "ACharmingFont";
         font-size: 16rem;
         color: #000;
-        text-shadow: 0px 0px 7px rgba(217, 174, 3, 1);
+        text-shadow: 0px 0px 10px rgba(217, 174, 3, 1);
         margin: 5rem 0;
         height: 100px;
         line-height: 7rem;
