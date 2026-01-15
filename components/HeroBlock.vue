4d8312601290cf9865876dd123596fe9a002b313
Csaba Szilagyi <mognoose@gmail.com>
2026-01-15 22:05:58 +0200

    add promopic

--- components/HeroBlock.vue
+++ components/HeroBlock.vue
@@ -1,5 +1,19 @@
 <template>
-    <div v-for="(node, index) in content" :key="index" class="hero" v-html="richTextFormat(node)" />
+    <a
+        v-if="content?.body?.content[0].data?.target?.metadata?.tags[0].sys.id === 'promo'"
+        v-for="(node, index) in content"
+        :href="content?.body?.content[0].data.target.fields.file.url"
+        :key="`promo-${index}`"
+        class="promo"
+        v-html="richTextFormat(node)"
+    />
+    <div
+        v-else
+        v-for="(node, index) in content"
+        :key="`logo-${index}`"
+        class="hero"
+        v-html="richTextFormat(node)"
+    />
 </template>
 
 <script setup>
@@ -44,6 +58,21 @@ function richTextFormat(text) {
         margin: 5rem 0;
         height: 100px;
         line-height: 7rem;
+    }
+}
+
+.promo {
+    text-align: center;
+    margin: auto;
+    display: grid;
+
+    :deep( .logo) {
+        width: 52rem;
+        max-width: calc(100dvw - 1.5rem);
+        transition-duration: 500ms;
+        margin: auto;
+        text-align: center;
+        margin-bottom: -4rem;
     }
 }
 </style>
